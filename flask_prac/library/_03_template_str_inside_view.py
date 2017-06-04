7a5ee5ad1139dea4d619925f592c1afd09e2cbb8
GarenLiang <ideabreeze@gmail.com>
2017-06-03 17:24:51 -0700

    Template

---
+++ flask_prac/library/_03_template_str_inside_view.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template_string
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    library_name = "Poe"
+    html = """
+        <html>
+            <h1>Welcome to {{library}} library!</h1>
+            <ul>
+                {% for author in authors %}
+                    <li>{{ author }}</li>
+                {% endfor %}
+            </ul>
+        </html>
+    """
+    authors = ["Edgar A. Poe", "Jorge L.Borges"]
+
+    rendered_html = render_template_string(
+        html, library=library_name,authors=authors
+    )
+    return rendered_html
