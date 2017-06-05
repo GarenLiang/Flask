5547dd134f812d0d92a8039d6bc778a7c8215aa3
GarenLiang <ideabreeze@gmail.com>
2017-06-04 18:59:06 -0700

    redirect

--- flask_prac/library/_08_redirects.py
+++ flask_prac/library/_08_redirects.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return redirect(url_for('request-info'))
+
+@app.route('/info')
+def info():
+    return redirect(url_for('request_info'), code=301)
 
 @app.route('/request-info')
 def request_info():
